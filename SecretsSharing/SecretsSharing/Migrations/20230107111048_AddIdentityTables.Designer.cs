518a50d49fd57d7beaea7f4ccd15940dd8b09e3f
Pavel <tankist3001-sergant@mail.ru>
2023-01-07 18:12:31 +0700

    1st migration applied, added and applied 2nd migrations

--- SecretsSharing/SecretsSharing/Migrations/20230107111048_AddIdentityTables.Designer.cs
+++ SecretsSharing/SecretsSharing/Migrations/20230107111048_AddIdentityTables.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SecretsSharing.Models;
 
 namespace SecretsSharing.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20230107111048_AddIdentityTables")]
+    partial class AddIdentityTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
