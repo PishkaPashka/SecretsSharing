620763e4d47ae774bda730c01b6ff7328fae0719
Pavel <tankist3001-sergant@mail.ru>
2023-01-07 18:09:59 +0700

    Added default realization for registration from documentation

---
+++ SecretsSharing/SecretsSharing/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SecretsSharing.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
