c065bd8a0f41568b66e45440260fc5dbb4079918
Pavel <tankist3001-sergant@mail.ru>
2023-01-07 17:49:25 +0700

    Added EF Core, EF.Tools, EF.SQLite, added initial migration

---
+++ SecretsSharing/SecretsSharing/ApplicationContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SecretsSharing
+{
+    public class ApplicationContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=secrets.db");
+        }
+    }
+}
